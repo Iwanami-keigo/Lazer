d102970d5c57a2a141efd86b040fb78016a9ad5c
keigoiwanami2 <oresaikou0529@gmail.com>
2019-12-24 22:30:09 +0900

    ”Lazer”７回目のコミット　2019/12/24　22：30時点の進捗
    ステージ開始時、クリア自のフェードイン・フェードアウト演出の実装

--- Assets/Script/UI/PanelController.cs
+++ Assets/Script/UI/PanelController.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class PanelController : MonoBehaviour {
 
 	private GameObject goal;
-	private float alfa = 0f;
+	private float alfa = 1f;
 	float red,green,blue;
-	public float speed = 0.01f;
+	public float speed = 0.0015f;
+	private GameObject back;
+	private GameObject next;
 
 
 	// Use this for initialization
@@ -17,16 +19,29 @@ public class PanelController : MonoBehaviour {
 		red = GetComponent<Image> ().color.r;
 		green = GetComponent<Image> ().color.g;
 		blue = GetComponent<Image> ().color.b;
+
+		back = GameObject.Find ("BackButton");
+		next = GameObject.Find ("NextButton");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GetComponent<Image> ().color = new Color (red,green,blue, alfa);
-		if(goal.tag == "GoalEnterTag"  && alfa <= 0.8f){
+		if (goal.tag != "GoalEnterTag") {
+			alfa -= speed;
+			if (alfa <= 0.001f) {
+				this.gameObject.SetActive (false);
+			}
+		}
 
+		if(goal.tag == "GoalEnterTag"  && alfa <= 0.5f){
+			
 				alfa += speed;
 
 	
 }
+		if (back.tag == "Buttonnull" || next.tag == "Buttonnull") {
+			alfa += speed;
+		}
 }
 }
