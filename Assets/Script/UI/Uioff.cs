eec508cbaa8e0b4b474e669d954b53a002d6f7d7
keigoiwanami2 <oresaikou0529@gmail.com>
2020-02-11 17:34:30 +0900

    "Lazer.".32 2020/02/11　17:30時点の進捗
    UIoffボタン制作、　～７ステージまで実装

---
+++ Assets/Script/UI/Uioff.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Uioff : MonoBehaviour
+{
+    private GameObject retry;
+    private GameObject joystick;
+    private GameObject home;
+    public GameObject hint;
+    public GameObject skip;
+
+    private bool downflag = false;
+    Image image;
+    private GameObject buttonimage;
+
+    public Sprite on;
+    public Sprite off;
+
+    string[] stages;
+    string nouwstage;
+    // Use this for initialization
+    void Start()
+    {
+        retry = GameObject.Find("Retry");
+        joystick = GameObject.Find("Joystick");
+        home = GameObject.Find("Homeback");
+        buttonimage = transform.FindChild("Button").gameObject;
+        image = buttonimage.GetComponent<Image>();
+        stages = new string[] { "Stage1", "Stage2", "Stage3", "Stage4", "Stage5",  "Stage7", "Stage8", "Stage9" };
+
+        nouwstage = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(downflag == false)
+        {
+            image.sprite = on;
+        }
+        else
+        {
+            image.sprite = off;
+        }
+    }
+    public void buttondown()
+    {
+        Debug.Log("押された");
+         if(downflag == false)
+        {
+            retry.SetActive(false);
+            joystick.SetActive(false);
+            home.SetActive(false);
+            hint.SetActive(false);
+            skip.SetActive(false);
+            downflag = true;
+        }
+        else
+        {
+            retry.SetActive(true);
+            joystick.SetActive(true);
+            home.SetActive(true);
+           
+            skip.SetActive(true);
+            downflag = false;
+            for (int i = 0; i < stages.Length; i++)
+            {
+              if(nouwstage == stages[i])
+                {
+                    hint.SetActive(true);
+                }
+
+            }
+          
+        }
+    }
+}
+    
+
+
